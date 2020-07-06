1b16a174b7bfbc541625ac9dcd4e8246469e1f71
katoy <youichikato@gmail.com>
2020-07-06 17:09:32 +0900

    rubocop 設定調整、警告への対処

--- spec/models/projecrt_spec.rb
+++ spec/models/projecrt_spec.rb
@@ -3,12 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  shared_context "project clear and reset pk" do
+  shared_context 'project clear and reset pk' do
     Project.destroy_all
     Project.reset_pk_sequence
   end
 
-  shared_context "project time_travel" do
+  shared_context 'project time_travel' do
     before do
       travel_to the_time
       freeze_time
@@ -19,7 +19,7 @@ RSpec.describe Project, type: :model do
     end
   end
 
-  shared_context "project create projects" do
+  shared_context 'project create projects' do
     let!(:projects) do
       ret = create_list(:project, 2)
       2.times do |idx|
@@ -43,22 +43,22 @@ RSpec.describe Project, type: :model do
     expect(project.errors[:name]).to include("can't be blank")
   end
 
-  context "#csv_name" do
-    subject{ Project.csv_name }
-    let(:the_time){Time.zone.parse('2020-01-02 08:59:59')}
-    include_context "project time_travel"
+  context '#csv_name' do
+    subject { Project.csv_name }
+    let(:the_time) { Time.zone.parse('2020-01-02 08:59:59') }
+    include_context 'project time_travel'
     let(:expect_csv_name) do
       "#{Rails.root}/csvs/projects_2020-01-02_08_59_59_000JST.csv"
     end
 
-    it{ is_expected.to eq expect_csv_name }
+    it { is_expected.to eq expect_csv_name }
   end
 
   context '#to_csv_by_sql' do
-    include_context "project clear and reset pk"
-    include_context "project create projects"
-    let!(:the_time){Time.zone.parse('2020-01-02 08:59:00')}
-    include_context "project time_travel"
+    include_context 'project clear and reset pk'
+    include_context 'project create projects'
+    let!(:the_time) { Time.zone.parse('2020-01-02 08:59:00') }
+    include_context 'project time_travel'
     before do
       File.delete(Project.csv_name) if File.exist?(Project.csv_name)
       Project.to_csv_by_sql
@@ -71,12 +71,12 @@ RSpec.describe Project, type: :model do
   end
 
   context '#to_csv' do
-    subject{Project.to_csv}
-    include_context "project clear and reset pk"
-    include_context "project create projects"
+    subject { Project.to_csv }
+    include_context 'project clear and reset pk'
+    include_context 'project create projects'
 
-    let!(:the_time){Time.zone.parse('2020-01-02 08:59:01')}
-    include_context "project time_travel"
+    let!(:the_time) { Time.zone.parse('2020-01-02 08:59:01') }
+    include_context 'project time_travel'
     before do
       File.delete(Project.csv_name) if File.exist?(Project.csv_name)
     end
@@ -87,21 +87,22 @@ RSpec.describe Project, type: :model do
       "2,Project 2,Test project 2.\n"
     end
 
-    context "check contens with file" do
-      it "contents of csv file" do
+    context 'check contens with file' do
+      it 'contents of csv file' do
         subject
         expect(File.read(Project.csv_name)).to eq expect_lines
       end
     end
 
-    context "check contents without file" do
+    context 'check contents without file' do
       let(:buffer) { StringIO.new }
       before do
-        allow(File).to receive(:open).with(Project.csv_name, 'w:UTF-8')
-                   .and_yield(buffer)
+        allow(File).to receive(:open)
+          .with(Project.csv_name, 'w:UTF-8')
+          .and_yield(buffer)
       end
 
-      it "contents of csv file" do
+      it 'contents of csv file' do
         subject
         expect(buffer.string).to eq expect_lines
       end
